466ba8066102c222413dd85acc06a8b6ddff22ca
pluiel <pluiel@outlook.com>
2020-06-05 17:54:16 +0900

    Initial Commit

---
+++ Piechart1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Piechart1
+//
+//  Created by Yuka Uematsu on 2020/06/05.
+//  Copyright © 2020 Uematsu Well. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
